7f5ce6d433b5cc2495b762e852af052246e4d156
Anthony Hsu <anthhsu@utexas.edu>
2022-10-12 22:39:55 -0500

    Standardize size of grid

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-divContainerColOne = document.createElement("div");
-divContainerColOne.classList.add("flex-container", "flex-container-col");
+let row = 16;
+let col = 16;
 
-divMainContainer = document.getElementById("container");
-divMainContainer.appendChild(divContainerColOne);
+const divMainContainer = document.getElementById("container");
 
 function generateGrid() {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < row; i++) {
         let divContainerRow = document.createElement("div");
         divContainerRow.classList.add("flex-container", "flex-container-row");
-        divContainerColOne.append(divContainerRow);
-        for (let j = 0; j < 4; j++) {
+        divMainContainer.append(divContainerRow);
+        for (let j = 0; j < col; j++) {
             let divSquare = document.createElement("div");
             divSquare.classList.add("square");
             divContainerRow.appendChild(divSquare);
